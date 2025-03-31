cfed44a5dfc18515317211e3250cc19d2db53abd
Giankaur03 <W0852088@myscc.ca>
2025-03-30 23:33:33 -0400

    created new branch for assignment 9.

--- app/src/main/java/com/example/assignment_6/ExpenseDetailsActivity.kt
+++ app/src/main/java/com/example/assignment_6/ExpenseDetailsActivity.kt
@@ -2,25 +2,21 @@ package com.example.assignment_6
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.assignment_6.databinding.ExpenseDetailsBinding
 
 class ExpenseDetailsActivity: AppCompatActivity() {
 
-    private lateinit var binding: ExpenseDetailsBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.expense_item)
 
 
-        binding = ExpenseDetailsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
         val expense = intent.getStringExtra("Expense_Name")
         val amount = intent.getStringExtra("Expense_Amount")
 
-        binding.DisplayExpence.text = "Expence Name: $expense"
-        binding.displayAmount.text = "Expense Amount: $amount"
+
 
 
     }
